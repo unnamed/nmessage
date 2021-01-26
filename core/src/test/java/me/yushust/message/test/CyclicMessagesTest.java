7637bf9b17c37153ffd1bae8813307ba71a0c6ed
Yushu <yushu@unnamed.team>
2021-01-25 22:32:25 -0500

    Fix tests: That behaviour isn't expected now

--- core/src/test/java/me/yushust/message/test/CyclicMessagesTest.java
+++ core/src/test/java/me/yushust/message/test/CyclicMessagesTest.java
@@ -1,6 +1,7 @@
 package me.yushust.message.test;
 
 import me.yushust.message.MessageProvider;
+import me.yushust.message.exception.MessageHandlingException;
 import me.yushust.message.test.base.HandlerTestCase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,9 +13,10 @@ public class CyclicMessagesTest extends HandlerTestCase {
 
     MessageProvider handler = MessageProvider.create(source);
 
-    Assertions.assertEquals("Check Check Check Check %path_linked-message-2%",
-        handler.get(null, "linked-message-1"));
-
+    Assertions.assertThrows(
+      MessageHandlingException.class,
+      () -> handler.get(null, "linked-message-1")
+    );
   }
 
 }
