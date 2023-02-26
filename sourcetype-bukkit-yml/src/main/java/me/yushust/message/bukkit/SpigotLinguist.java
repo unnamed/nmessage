6a9badb60c44ed988c41bed7178939e269829f2f
Call4han <ccasarider@gmail.com>
2023-02-26 15:45:58 -0500

    fix: fix getLocale conflict in current versions.

--- sourcetype-bukkit-yml/src/main/java/me/yushust/message/bukkit/SpigotLinguist.java
+++ sourcetype-bukkit-yml/src/main/java/me/yushust/message/bukkit/SpigotLinguist.java
@@ -3,6 +3,9 @@ package me.yushust.message.bukkit;
 import me.yushust.message.language.Linguist;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Implementation of {@link Linguist} for
  * Bukkit's {@link Player}. Gets its language
@@ -15,10 +18,33 @@ import org.bukkit.entity.Player;
  */
 public class SpigotLinguist
         implements Linguist<Player> {
+    private final static Method GET_LOCALE;
+
+    static {
+        Method getLocale = null;
+        try {
+            getLocale = Player.class.getMethod("getLocale");
+        } catch (NoSuchMethodException ignored) {
+            // Ignore
+        }
+        GET_LOCALE = getLocale;
+
+    }
 
     @Override
     public String getLanguage(Player player) {
-        return player.spigot().getLocale().split("_")[0];
+
+        if (GET_LOCALE == null) {
+            return player.spigot().getLocale().split("_")[0];
+        } else {
+            try {
+                return ((String) GET_LOCALE.invoke(player)).split("_")[0];
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new IllegalStateException("Failed to get locale from player", e);
+            }
+        }
+
+
     }
 
 }
