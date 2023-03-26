5db15b63c09c270ebdef0a0c0aae70e67bc2bc2c
yusshu <yushuverduras1309@gmail.com>
2023-03-26 17:15:27 -0500

    tests(sourcetype-gson-json): add tests for non existent messages

--- sourcetype-gson-json/src/test/java/me/yushust/message/source/json/ResourceMessageGetTest.java
+++ sourcetype-gson-json/src/test/java/me/yushust/message/source/json/ResourceMessageGetTest.java
@@ -6,26 +6,41 @@ import me.yushust.message.impl.MessageProviderImpl;
 import me.yushust.message.source.MessageSource;
 import me.yushust.message.source.MessageSourceDecorator;
 import me.yushust.message.source.gson.JsonResourceSource;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ResourceMessageGetTest {
 
-    @Test
-    public void test() {
-        MessageSource source = MessageSourceDecorator
+    private final MessageSource source;
+    private final MessageProvider provider;
+
+    public ResourceMessageGetTest() {
+        this.source = MessageSourceDecorator
                 .decorate(new JsonResourceSource(
                         getClass().getClassLoader(),
                         "test_%lang%.json"
                 )).addFallbackLanguage("en")
                 .get();
+        this.provider = new MessageProviderImpl(source, new ConfigurationHandle());
+    }
 
-        MessageProvider provider = new MessageProviderImpl(source, new ConfigurationHandle());
-
+    @Test
+    public void test() {
         assertEquals("nefasto", provider.get(null, "test1"));
         assertEquals("terrible", provider.get(null, "test2"));
         assertEquals("oremos", provider.get(null, "test3"));
         assertEquals("áéíóú ñ §æµŋnamed", provider.get(null, "test4.test5"));
     }
+
+    @Test
+    @DisplayName("Test message not found")
+    public void test_message_not_found() {
+        assertEquals("thisdoesnotexist", provider.get(null, "thisdoesnotexist"));
+        assertEquals("this.does.not.exist", provider.get(null, "this.does.not.exist"));
+        assertEquals("test4.thisdoesnotexist", provider.get(null, "test4.thisdoesnotexist"));
+        assertEquals("test4.this.does.not.exist", provider.get(null, "test4.this.does.not.exist"));
+    }
+
 }
